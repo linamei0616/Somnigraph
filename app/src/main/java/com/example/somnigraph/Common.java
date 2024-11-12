93abf19fcb9589b1b5a67bc53e734e4b9c9528e7
Lina Mei <linamei0616@gmail.com>
2024-11-11 23:31:00 -0600

    made file for common functions

---
+++ app/src/main/java/com/example/somnigraph/Common.java
@@ -0,0 +1,30 @@
+// This file is used for functions that is commonly shared among all views, such as spinners or nav bars
+
+package com.example.somnigraph;
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+public class Common {
+    // set up the Spinner with tags
+    public static void setupSpinner(Activity activity, Spinner spinner, DreamManager dreamManager) {
+        // populate the Spinner with sorted tags from the DreamManager
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(activity, android.R.layout.simple_spinner_item, dreamManager.getSortedTagsByFrequency());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    // helper method to set up the navigation bar
+    public static void setupNavBar(Activity activity) {
+        ImageButton cloudBtn = activity.findViewById(R.id.cloudButton);
+        ImageButton pencilBtn = activity.findViewById(R.id.pencilButton);
+        ImageButton calendarBtn = activity.findViewById(R.id.calendarButton);
+
+        // click listeners for the buttons
+        cloudBtn.setOnClickListener(v -> activity.startActivity(new Intent(activity, WordCloudActivity.class)));
+        pencilBtn.setOnClickListener(v -> activity.startActivity(new Intent(activity, MainActivity.class)));
+        calendarBtn.setOnClickListener(v -> activity.startActivity(new Intent(activity, CalendarActivity.class)));
+    }
+}
